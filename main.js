aebe0959b3b5c0cc790d58c1eb6a62e557ef9c47
Ayoub Fellat <paypal1business.over@gmail.com>
2021-03-09 11:15:50 +0100

    Add Question Object To main.js

---
+++ main.js
@@ -0,0 +1,50 @@
+const quizData = [
+    {
+        question: "The Best Footbal Player in The World",
+        a: 'Messi',
+        b: 'C.Ronaldo',
+        c: 'Maradona',
+        d: 'Neymar',
+        correct: 'c'
+    },
+    {
+        question: "The Best Actress in The World",
+        a: 'Marie Avgeropoulos',
+        b: 'Eliza Taylor',
+        c: 'Emma Stone',
+        d: 'Lindsey Morgan',
+        correct: 'c' 
+    },
+    {
+        question: "What is the most used programming language in 2020",
+        a: 'Java',
+        b: 'C',
+        c: 'Python',
+        d: 'JavaScript',
+        correct: 'd' 
+    },
+    {
+        question: "Who is the President Of US?",
+        a: 'Joe Biden',
+        b: 'Donald Trum',
+        c: 'Ayoub Fellat',
+        d: 'Obama',
+        correct: 'a' 
+    },
+    {
+        question: "What does HTML Stand For?",
+        a: 'Hypertext Markup Language',
+        b: 'Cascading Style Sheet',
+        c: 'Jason Object Notation',
+        d: 'Document Object Model',
+        correct: 'a' 
+    },
+    {
+        question: "What year was JavaScript?",
+        a: '1996',
+        b: 'none of the above',
+        c: '1995',
+        d: '2000',
+        correct: 'b' 
+    },
+];
