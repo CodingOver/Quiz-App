64072cb41867647d4ed410e65d0ca8c6408e6420
Ayoub Fellat <paypal1business.over@gmail.com>
2021-03-09 17:18:30 +0100

    update HTML Classes And Id & Added Function Check Correct Answer & Final Result & Reload Page

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ const quizData = [
         b: 'Eliza Taylor',
         c: 'Emma Stone',
         d: 'Lindsey Morgan',
-        correct: 'c' 
+        correct: 'b' 
     },
     {
         question: "What is the most used programming language in 2020",
@@ -48,3 +48,70 @@ const quizData = [
         correct: 'b' 
     },
 ];
+
+const quiz = document.getElementById('quiz');
+const answerEls = document.querySelectorAll('.answer');
+const questionEl = document.getElementById('question');
+const a_text = document.getElementById('a_text');
+const b_text = document.getElementById('b_text');
+const c_text = document.getElementById('c_text');
+const d_text = document.getElementById('d_text');
+const submitBtn = document.getElementById('submit');
+
+
+let currentQuiz = 0;
+let score = 0;
+
+loadQuiz();
+
+function loadQuiz() {
+
+    deselectedAnswers();
+    
+    const currentQuizData = quizData[currentQuiz];
+    
+    questionEl.textContent = currentQuizData.question;
+    a_text.textContent = currentQuizData.a;
+    b_text.textContent = currentQuizData.b;
+    c_text.textContent = currentQuizData.c;
+    d_text.textContent = currentQuizData.d;
+}
+
+function getSelected() {
+    let answer = undefined;
+
+    answerEls.forEach(answerEl => {
+        if (answerEl.checked) {
+            answer = answerEl.id;
+        }
+    });
+
+    return answer;
+}
+
+function deselectedAnswers() {
+    answerEls.forEach(answerEl => {
+        answerEl.checked = false;
+    });
+}
+
+
+
+submitBtn.addEventListener('click', () => {
+    // check to see answer
+    const answer = getSelected();
+    if (answer) {
+        if (answer === quizData[currentQuiz].correct) {
+            score++;
+        }
+        currentQuiz++;
+        if (currentQuiz < quizData.length) {
+            loadQuiz();
+        } else {
+            quiz.innerHTML = `
+                <h2 class="finalResult">You Answered Correctly At ${score}/${quizData.length} Questions.</h2>
+                <button onclick="location.reload()">Reload</button>
+                `;
+        }
+    }
+});
